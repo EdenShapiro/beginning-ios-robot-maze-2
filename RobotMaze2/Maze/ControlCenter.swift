bf9c186cdcc354b5931a54212c9963f9d4134a22
Gabrielle Miller-Messner <gabriellemiller-messner@Gabrielles-MacBook-Pro.local>
2015-11-30 16:48:38 -0800

    first commit, part 2

--- RobotMaze2/Maze/ControlCenter.swift
+++ RobotMaze2/Maze/ControlCenter.swift
@@ -12,18 +12,57 @@ class ControlCenter {
     var mazeController: MazeController!
 
     func moveComplexRobot(robot: ComplexRobotObject) {
+      
+    // You may want to paste your Project 2.1 implementation of moveComplexRobot() here
         
-        // Step 1.1c
-        // TODO: Call the function, isFacingWall(), and define a constant to be equal to its return value. You can use the suggested constant name below--uncomment the code and add the function call.
-        //        let robotIsBlocked =
+        // Step 2.1c
+        // TODO: Save the return value of checkWalls() to a constant called myWallInfo.
         
         
-        // Step 1.1d
-        // TODO: Test the isFacingWall() function. Be sure to comment out or delete your test code once you are finished testing!
+        // Step 2.2a
+        // Categorize the robot's current location based on the number of walls
+        
+        // let isThreeWayJunction = (myWallInfo.numberOfWalls == 1)
+        
+        // TODO: Define the constant, isTwoWayPath
+        
+        // TODO: Define the constant, isDeadEnd
+        
+        
+        // Step 2.2b
+        // Test whether the values of the above constants are correct
+        
+        
+        // Step 2.3a
+        // Three-way Path - else-if statements
+        
+        // TODO: If the robot encounters a three way junction and there IS a wall ahead, it should randomly rotate right or left. Uncomment the code below.
+        //        if isThreeWayJunction && robotIsBlocked {
+        //            randomlyRotateRightOrLeft(myRobot)
+        //        }
+        
+        
+        // Step 2.3b
+        // TODO: If the robot encounters a three way junction and there is NO wall ahead, it should continue straight or rotate (you need to write this else-if statement)
+        
+        
+        // Step 2.3c
+        // Two-way Path - else-if statements
+        
+        // TODO: If the robot encounters a two way path and there is NO wall ahead it should continue forward.
+        
+        // TODO: If the robot encounters a two way path and there IS a wall ahead, it should randomly rotate.
+        
+        
+        // Step 2.3d
+        // Dead end - else-if statements
+        
+        // TODO: If the robot encounters a dead end and there is NO wall ahead it should move forward.
+        
+        // TODO: If the robot encounters a dead end and there IS a wall ahead it should rotateRight().
     }
-        
+    
     func previousMoveIsFinished(robot: ComplexRobotObject) {
             self.moveComplexRobot(robot)
     }
-    
 }
--- RobotMaze2/Maze/RobotTurningInstructions.swift
+++ RobotMaze2/Maze/RobotTurningInstructions.swift
@@ -13,13 +13,13 @@ extension ControlCenter {
     func randomlyRotateRightOrLeft(robot: ComplexRobotObject) {
         let randomNumber = arc4random() % 2
         
-        // Step 1.2
-        // TODO: Write an if statement that randomly calls either robot.rotateRight() or robot.rotateLeft() (based on the value of the randomNumber constant)
+        // You may want to paste your Project 2.1 implementation of randomlyRotateRightOrLeft(robot: ComplexRobotObject) here
     }
     
     func continueStraightOrRotate(robot: ComplexRobotObject) {
         let randomNumber = arc4random() % 2
-        // Step 1.3
-        // TODO: Write an if statement that randomly calls either robot.move() or randomlyRotateRightOrLeft(robot: ComplexRobotObject)
+        
+        // You may want to paste your Project 2.1 implementation of continueStraightOrRotate(robot: ComplexRobotObject) here
+        
     }
 }

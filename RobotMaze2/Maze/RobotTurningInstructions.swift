ecabb7f6e7b74a64828804155baa4b35b5f22760
Gabrielle Miller-Messner <gabriellemiller-messner@Gabrielles-MacBook-Pro.local>
2015-12-03 13:54:09 -0800

    adding more comments for part 3

--- RobotMaze2/Maze/RobotTurningInstructions.swift
+++ RobotMaze2/Maze/RobotTurningInstructions.swift
@@ -19,7 +19,20 @@ extension ControlCenter {
     func continueStraightOrRotate(robot: ComplexRobotObject) {
         let randomNumber = arc4random() % 2
         
-        // You may want to paste your Part 1 implementation of continueStraightOrRotate(robot: ComplexRobotObject) here
+        // You may want to paste your Part 1 implementation of continueStraightOrRotate(robot: ComplexRobotObject) here.
         
+        // Part 3 - Step 3.2
+        // TODO: Instead of calling randomlyRotateRightOrLeft() call turnTowardClearPath() when the robot has randomly chosen to rotate.
+    }
+    
+    func turnTowardClearPath(robot: ComplexRobotObject, wallInfo: (up: Bool, right: Bool, down: Bool, left: Bool, numberOfWalls: Int)) {
+        
+        // Part 3 - Step 3.1
+        // TODO: Tell the robot which way to turn toward the clear path. There are four cases where the robot should rotate to the right (the first two have been done for you--uncomment the code below). Write the remaining two cases where the robot should rotate to the right. For all other cases, the robot should rotate to the left.
+        //        if robot.direction == .Left && wallInfo.down {
+        //            robot.rotateRight()
+        //        } else if robot.direction == .Up && wallInfo.left {
+        //            robot.rotateRight()
+        //        }
     }
 }
